f67d173cc785d3d6389d09ae7ed9a4280744a370
Louiza Mak <louiza.mak@gmail.com>
2024-05-06 11:27:33 -0500

    complete lab

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,11 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"John Cena|Anya Taylor-Joy|D'Angelo"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"\({0,1}[0-9]{3}\){0,1}[- ]{0,1}[0-9]{3}[-]{0,1}[0-9]{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"[A-z][A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}"
 email_regex = re.compile(email_address)
